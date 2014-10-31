5574a0f774d18651abdb8f2eb32f71690197533b
vaughan <vaughanp@solutionsvp.ca>
2014-10-30 20:47:14 -0400

    communicate with server to update data from ui-grid

--- server.js
+++ server.js
@@ -35,15 +35,27 @@ app.use('/table', function(req, res){
 app.use('/sql', function(req, res){
   var sql = url.parse(req.url, true).query['get'];
   console.log('sql: ' + sql);
-  db.sqlite3_db.all(sql, function(err, rows) {
-    if (err) {
-      console.log('error: ' + err);
-      console.log(' - sql: ' + sql);
-     }
-    console.log(rows);
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(rows, null, 3));
-   });
+  res.setHeader('Content-Type', 'application/json');
+  if (sql.match('^select')) { //select queries
+    db.sqlite3_db.all(sql, function(err, rows) {
+      if (err) {
+        console.log('error: ' + err);
+        console.log(' - sql: ' + sql);
+       }
+      console.log(rows);
+      res.end(JSON.stringify(rows, null, 3));
+     });
+   } else { //update queries
+    db.sqlite3_db.run(sql, [], function(err) {
+      if (err) {
+        console.log('error: ' + err);
+        console.log(' - sql: ' + sql);
+        res.end(JSON.stringify(err, null, 3));
+       } else {
+		res.end(JSON.stringify({result:'success'}), null, 3);
+	   }
+     }); 
+   }
  });
 
 app.use('/tables', function(req, res) {
